b23921f5c979a56c5d9349a512eb406a14b88480
Taufik Rahman <taufik.rahmanwidodo@gmail.com>
2026-03-05 14:22:40 +0700

    refactor: update repository structure to remove interfaces and implement transaction management

--- repository/tx_manager.go
+++ repository/tx_manager.go
@@ -26,25 +26,18 @@ func (t *txManager) WithTransaction(ctx context.Context, fn models.RunInTransact
 		return tx.Error
 	}
 
-	// Sisipkan object transaksi (tx) ke dalam context yang baru
 	ctxWithTx := context.WithValue(ctx, TxContextKey, tx)
 
-	// Jalankan fungsi (service logic) menggunakan context yang sudah ada transaksinya
 	err := fn(ctxWithTx)
 
-	// Jika terjadi error dari fungsi, Rollback
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
 
-	// Jika sukses, Commit
 	return tx.Commit().Error
 }
 
-// --- HELPER UNTUK REPOSITORY ---
-// Fungsi ini akan mengekstrak DB dari context jika ada transaksi berjalan.
-// Jika tidak ada transaksi, ia akan menggunakan DB biasa.
 func GetDB(ctx context.Context, defaultDB *gorm.DB) *gorm.DB {
 	if tx, ok := ctx.Value(TxContextKey).(*gorm.DB); ok {
 		return tx
